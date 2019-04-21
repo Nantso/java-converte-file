cdb59f7d3b85945093120cdfb8e9201c655be2dd
antso <nyantsorakotondrabe@gmail.com>
2019-04-21 23:51:56 +0300

    add attribute on conversion

--- Converter.java
+++ Converter.java
@@ -12,27 +12,31 @@ import java.util.logging.Logger;
 public class Converter{
 
 private static final Logger LOG = Logger.getLogger(Converter.class.getName());
+public static void conversion(String pathname){
+    try {
+        String line;
+        String mp4File = pathname;
+        String mp3File = "/home/nyantso/Bureau/audio.mp3";
+
+        String cmd = "ffmpeg -i " + mp4File + " " + mp3File;
+        Process p = Runtime.getRuntime().exec(cmd);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(p.getErrorStream()));
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+        }
+        p.waitFor();
+        System.out.println("Video converted successfully!");
+        in.close();
+    } catch (IOException | InterruptedException e) {
+        LOG.log(Level.SEVERE, null, e);
+    }
+}
 
     public static void main(String[] args) {
         
-        try {
-            String line;
-            String mp4File = "/home/nyantso/Bureau/video.mp4";
-            String mp3File = "/home/nyantso/Bureau/audio.mp3";
-
-            String cmd = "ffmpeg -i " + mp4File + " " + mp3File;
-            Process p = Runtime.getRuntime().exec(cmd);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(p.getErrorStream()));
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
-            p.waitFor();
-            System.out.println("Video converted successfully!");
-            in.close();
-        } catch (IOException | InterruptedException e) {
-            LOG.log(Level.SEVERE, null, e);
-        }
+       String pathname = args[0];
+       conversion(pathname);
 
     }
 }
